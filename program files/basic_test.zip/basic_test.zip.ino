720d8e81581cf2756d48618aaf5cb60c1ede4f52
Jonathan <jjonathanlow@gmail.com>
2020-05-18 03:48:06 -0700

    tested: emiters/recivers, Buzzer, buttons, LEDS

--- program files/basic_test.zip/basic_test.zip.ino
+++ program files/basic_test.zip/basic_test.zip.ino
@@ -1,22 +1,38 @@
 #define BUZZ 13
 #define EMIT0 22
 #define RECIV0 23
+#define EMIT1 20
+#define RECIV1 21
+#define EMIT3 17
+#define RECIV3 16
+#define EMIT2 15
+#define RECIV2 14
+
+#define BUTTON1 11
+#define BUTTON2 12
 void setup() {
   pinMode(BUZZ, OUTPUT);
   pinMode(EMIT0, OUTPUT);
   pinMode(RECIV0, INPUT);
+  pinMode(EMIT1, OUTPUT);
+  pinMode(RECIV1, INPUT);
+  pinMode(RECIV3, INPUT);
+  pinMode(EMIT3, OUTPUT);
+  pinMode(RECIV2, INPUT);
+  pinMode(EMIT2, OUTPUT);
+  pinMode(BUTTON1, INPUT);
+  pinMode(BUTTON2, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(EMIT0, HIGH);
-  //digitalWrite(BUZZ, HIGH);
-  delay(2);
-  int dist = analogRead(RECIV0);
-  digitalWrite(EMIT0, LOW);
-  Serial.println(dist);
-  //digitalWrite(BUZZ, LOW);
+  int bt1 = digitalRead(BUTTON1);
+  int bt2 = digitalRead(BUTTON2);
+  Serial.print("Button1: ");
+  Serial.println(bt1);
+  Serial.print("Button2: ");
+  Serial.println(bt2);
   delay(50);
 
 }
